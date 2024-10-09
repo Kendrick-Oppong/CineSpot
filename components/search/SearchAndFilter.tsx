ba3b17717b98746debf6f67dc18f162640e04d9c
Kendrick <kendrickoppong94@gmail.com>
2024-10-09 21:51:11 +0000

    feat:add "Load More" functionality to display movies incrementally

--- components/search/SearchAndFilter.tsx
+++ components/search/SearchAndFilter.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { MovieApiResponse } from "@/types";
 import { Card } from "../card";
 import { useState } from "react";
@@ -10,11 +11,20 @@ const SearchAndFilter = ({
   initialMovies: MovieApiResponse[];
 }) => {
   const [title, setTitle] = useState("");
+  const [visibleCount, setVisibleCount] = useState(10); // Number of movies visible
 
   // Filter movies based on the title entered by the user
   const filteredMovies = initialMovies.filter((movie) =>
     movie.title.toLowerCase().includes(title.trim().toLowerCase())
   );
+
+  // Slice movies based on visible count
+  const visibleMovies = filteredMovies.slice(0, visibleCount);
+
+  // Function to load more movies
+  const loadMoreMovies = () => {
+    setVisibleCount((prevCount) => prevCount + 10); // Load 10 more movies
+  };
 
   return (
     <>
@@ -31,12 +41,24 @@ const SearchAndFilter = ({
 
       {/* Display filtered movies */}
       <div className="grid grid-cols-auto lg:grid-cols-auto-lg gap-4">
-        {filteredMovies.length > 0 ? (
-          filteredMovies.map((movie) => <Card key={movie.id} movie={movie} />)
+        {visibleMovies.length > 0 ? (
+          visibleMovies.map((movie) => <Card key={movie.id} movie={movie} />)
         ) : (
           <p className="text-destructive">No movies found</p>
         )}
       </div>
+
+      {/* Load More button */}
+      {visibleMovies.length < filteredMovies.length && (
+        <div className="flex justify-center mt-4">
+          <Button
+            onClick={loadMoreMovies}
+            className="px-4 py-2 bg-primary text-white rounded"
+          >
+            Load More
+          </Button>
+        </div>
+      )}
     </>
   );
 };
